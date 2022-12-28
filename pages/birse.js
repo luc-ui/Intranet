e13775657ac985a6eda405b68bf384bcb36a43fd
lucas <113012326+luc-ui@users.noreply.github.com>
2022-12-28 19:55:14 +0100

    Delete pages directory

--- pages/birse.js
+++
@@ -1,7 +0,0 @@
-import Publis from '../components/Publis'
-
-export default function Birse({ selectStrass }) {
-	const authStrass = ['AE','cvis']
-
-	return <Publis type="birse" name="Birse - Bien-être" authStrass={authStrass} selectStrass={selectStrass}/>
-}
